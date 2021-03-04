d007c3462245393ec723a4e116f41f461ba6dfe7
ethanwaggoner <74027222+ethanwaggoner@users.noreply.github.com>
2021-03-04 16:58:59 -0500

    Add files via upload

---
+++ fileTypes.py
@@ -0,0 +1,66 @@
+import docx2txt
+import PyPDF2
+import pandas as pd
+
+
+# Extracts the data from the Word document
+def word_sort(directory):
+    print(directory)
+    try:
+        data = docx2txt.process(directory)
+        return data
+    except:
+        print("Error retrieving data")
+
+
+# Extracts the data from the CSV file
+def csv_sort(directory):
+    print(directory)
+    try:
+        data = str(pd.read_csv(directory))
+        return data
+    except:
+        print("Error retrieving data")
+
+
+# Extracts the data from the Excel file
+def excel_sort(directory):
+    print(directory)
+    try:
+        spreadsheet = pd.ExcelFile(directory, engine="openpyxl")
+        data_list = []
+        for sheet in spreadsheet.sheet_names:
+            data = str(spreadsheet.parse(sheet))
+            data_list.append(data)
+        return data_list
+    except:
+        print("Error retrieving data")
+
+
+# Extracts the data from the Text file
+def text_sort(directory):
+    print(directory)
+    try:
+        data_list = []
+        with open(directory, mode='r') as f:
+            for line in f:
+                data_list.append(line)
+            return data_list
+    except:
+        print("Error retrieving data")
+
+
+# Extracts the data from the PDF document
+def pdf_sort(directory):
+    print(directory)
+    try:
+        with open(directory, mode='rb') as file:
+            reader = PyPDF2.PdfFileReader(file)
+            data_list = []
+            for page in reader.pages:
+                pdf_text = page.extractText()
+                pdf_text = pdf_text.replace('\n', '')
+                data_list.append(pdf_text)
+            return data_list
+    except:
+        print("Error retrieving data")
---
+++ piiPatternMatching.py
@@ -0,0 +1,53 @@
+import re
+
+
+# Validates if the credit card number is legitimate using Luhn's algorithm
+def luhn(n):
+    r = [int(ch) for ch in str(n)][::-1]
+    return (sum(r[0::2]) + sum(sum(divmod(d*2, 10)) for d in r[1::2])) % 10 == 0
+
+
+# Return the censored version of the PII. Censors everything except dashes and the last 4 characters
+def censor(pii_list):
+    pii_return = []
+
+    for pii in pii_list:
+
+        to_censor = pii[:-4]
+        uncensored = pii[-4:]
+        censored = []
+
+        for char in to_censor:
+            if char != "-":
+                char = "*"
+                censored.append(char)
+            else:
+                censored.append(char)
+
+        censored_word = "".join(censored) + uncensored
+        pii_return.append(censored_word)
+
+    return pii_return
+
+
+# Runs regex against the data looking for Social Security Numbers
+def ssn(data):
+    pii_list = re.findall(r"""\b\d{3}-\d{2}-\d{4}\b|\b\d{3}\s\d{2}\s\d{4}\b|\b\d{3}-\d{6}\b""", data)
+
+    return censor(pii_list)
+
+
+# Runs regex against the data looking for Credit Card Numbers
+def cc(data):
+    pii_list = re.findall(r"\b\d{4}\s\d{4}\s\d{4}\s\d{4}\b|\b\d{16}\b|\b\d{4}-\d{4}-\d{4}-\d{4}\b", data)
+
+    return censor(pii_list)
+
+
+# Runs regex against the data looking for Maryland Drivers License IDs
+def drivers_md(data):
+    pii_list = re.findall(r"\b[a-zA-Z]\d{12}\b|\b[a-zA-z]-\d{3}-\d{3}-\d{3}-\d{3}\b", data)
+
+    return censor(pii_list)
+
+
